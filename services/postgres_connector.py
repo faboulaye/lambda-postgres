1d430f6c3529d4f8c21434db27ee042ae3c57556
Firdaws Aboulaye <faboulaye@generixgroup.com>
2020-02-12 18:37:51 +0100

    Upate connector

--- services/postgres_connector.py
+++ services/postgres_connector.py
@@ -3,16 +3,18 @@ import sys
 import psycopg2
 import logging
 
-# rds settings
-rds_host = os.environ.get('RDS_HOST')
-rds_username = os.environ.get('RDS_USERNAME')
-rds_user_pwd = os.environ.get('RDS_USER_PWD')
-rds_db_name = os.environ.get('RDS_DB_NAME')
-
-
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+# rds settings
+rds_host = os.getenv('RDS_HOST', 'localhost')
+rds_username = os.getenv('RDS_USER', 'postgres')
+rds_user_pwd = os.getenv('RDS_PWD', 'admin')
+rds_db_name = os.getenv('RDS_DB', 'rds')
+
+logger.info("Host: %s", rds_host)
+logger.info("User: %s", rds_username)
+logger.info("DB: %s", rds_db_name)
 try:
     conn_string = "host=%s user=%s password=%s dbname=%s" % \
         (rds_host, rds_username, rds_user_pwd, rds_db_name)
@@ -25,15 +27,11 @@ logger.info("SUCCESS: Connection to RDS Postgres instance succeeded")
 
 
 def lambda_handler(event, context):
-    query = """select id, name, job_title
-            from employee
-            order by 1"""
-
-    with conn.cursor() as cur:
+    with conn.cursor() as cursor:
         rows = []
-        cur.execute(query)
-        for row in cur:
-            logger.info(row)
+        cursor.execute("select * from employe")
+        for row in cursor:
+            print(row)
             rows.append(row)
 
-    return {'statusCode': 200, 'body': rows}
+        return {'statusCode': 200, 'body': rows}
